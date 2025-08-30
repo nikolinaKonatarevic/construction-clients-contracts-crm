b6ceedae62b856722064063e91b35e8e91384836
nikolinaKonatarevic <niki.konatarevic@gmail.com>
2025-08-30 14:28:03 +0200

    Initial commit

---
+++ src/main/java/com/Gradjevinsko_preduzece/GradjevinskoPreduzeceApplication.java
@@ -0,0 +1,13 @@
+package com.Gradjevinsko_preduzece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradjevinskoPreduzeceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradjevinskoPreduzeceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Gradjevinsko_preduzece/GradjevinskoPreduzeceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gradjevinsko_preduzece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradjevinskoPreduzeceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
